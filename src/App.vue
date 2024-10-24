b2062780d91a3090c04483b7b7f1129ba4182b48
Sumanth Reddy <sumanthcob@gmail.com>
2024-10-23 23:40:41 -0400

    binding input to ref

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const search = ref('')
+</script>
 
 <template>
   <h1>MeteoScope</h1>
-  <input placeholder="Enter a location 📍" />
+  <input v-model="search" type="text" placeholder="Enter a location 📍" />
 </template>
