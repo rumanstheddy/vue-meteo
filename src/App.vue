0219fd0661177cc5d06dcaff37e6a75ec9a0e4a5
Sumanth Reddy <sumanthcob@gmail.com>
2024-10-25 21:23:51 -0400

    styling home

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,13 @@ const search = ref('')
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <h1 class="text-4xl">MeteoScope</h1>
-    <input v-model="search" type="text" placeholder="Enter a location 📍" />
+  <div class="flex flex-col gap-6 justify-center">
+    <h1 class="text-4xl text-center">Meteoscope</h1>
+    <input
+      class="rounded-sm text-center p-2"
+      v-model="search"
+      type="text"
+      placeholder="Enter a location 📍"
+    />
   </div>
 </template>
