9ca94911f2ede3598fd87874282943e21f7913a8
Sumanth Reddy <sumanthcob@gmail.com>
2024-10-26 20:17:49 -0400

    removed base css styling

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,15 @@ const search = ref('')
 </script>
 
 <template>
-  <div class="flex flex-col gap-6 justify-center">
-    <h1 class="text-4xl text-center">Meteoscope</h1>
-    <input
-      class="rounded-sm text-center p-2"
-      v-model="search"
-      type="text"
-      placeholder="Enter a location 📍"
-    />
+  <div class="flex flex-col justify-center items-center h-[100vh]">
+    <div class="flex flex-col gap-6 max-w-md">
+      <h1 class="text-4xl text-center">Meteoscope</h1>
+      <input
+        class="rounded-md text-center p-2 border-2 border-neutral-300"
+        v-model="search"
+        type="text"
+        placeholder="Enter a location 📍"
+      />
+    </div>
   </div>
 </template>
